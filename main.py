6c2989d9611106cb8379c077eb54f439e32cc847
megane <susini.megane04@gmail.com>
2023-02-17 18:46:04 +0100

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+# the word to guess
+theWord = "tableau"
+theWord = list(theWord)
+
+# the letters already used
+usedLetters = []
+
+# the usable letters
+import string
+usableLetters = string.ascii_uppercase
+usableLetters = list(usableLetters)
+
+
